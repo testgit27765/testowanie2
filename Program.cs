89245303c1b34a32f1740265d2bb87bfd99caa16
Dorand Dorandus <Instusa91@wp.pl>
2019-10-27 15:23:07 +0100

    ^X
    zmiana

--- Program.cs
+++ Program.cs
@@ -6,7 +6,7 @@ namespace TestApka1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Helloorld!");
         }
     }
 }
