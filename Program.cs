db83477d33a0d8a4954aab28104c6916000869bc
Dorand Dorandus <Instusa91@wp.pl>
2019-10-27 15:46:57 +0100

    Dodany do brancza 2

--- Program.cs
+++ Program.cs
@@ -7,6 +7,7 @@ namespace TestApka1
         static void Main(string[] args)
         {
             Console.WriteLine("Hello world!");
+            Console.WriteLine("Hello world!");
         }
     }
 }
