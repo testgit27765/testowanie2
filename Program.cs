bc97a16eb35f602c28fc5b0e5559adc0a7aa96e8
Dorand Dorandus <Instusa91@wp.pl>
2019-10-27 15:10:01 +0100

    'prograiprogram'

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace TestApka1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
